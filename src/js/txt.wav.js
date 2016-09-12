11f40585ae86f5872517c05af617e3ef372f4b07
George Mayorga <georgetmayorga@gmail.com>
2016-09-12 16:32:45 -0400

    Add javascript style checking with JSHint

--- src/js/txt.wav.js
+++ src/js/txt.wav.js
@@ -1,5 +1,15 @@
 (function() {
+  'use strict';
+
   var textWaveElements = document.getElementsByClassName('txtwav');
+
+  function spanWrapHelper(el, text) {
+    for(var i = 0; i < text.length; i++) {
+      var span = document.createElement('span');
+      span.innerHTML = text[i] === ' ' ? '&nbsp;' : text[i];
+      el.appendChild(span);
+    }
+  }
 
   for (var i = 0, length = textWaveElements.length; i < length; i++) {
     var el = textWaveElements[i],
@@ -7,13 +17,5 @@
 
     el.innerHTML = '';
     spanWrapHelper(el, text);
-  }
-
-  function spanWrapHelper(el, text) {
-    for(var i = 0; i < text.length; i++) {
-      var span = document.createElement('span');
-      span.innerHTML = text[i] === " " ? "&nbsp;" : text[i];
-      el.appendChild(span);
-    }
   }
 }());
